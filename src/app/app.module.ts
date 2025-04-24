5b5e936e5b12a592cf8cc3f77e6571f2e9a3fc00
Florian <dark.joosh@gmail.com>
2025-04-24 19:36:11 +0200

    add product-list-grid.component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ProductService} from './services/product.service';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
